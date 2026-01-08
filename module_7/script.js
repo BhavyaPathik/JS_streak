df6e0b374b3d8cdcb9310eb5a3e50d4ef2b47ea5
QuasarX <pathik.bhavya@gmail.com>
2026-01-08 22:59:36 +0530

    Module 7 DOM and function

---
+++ module_7/script.js
@@ -0,0 +1,17 @@
+//DOM is Document object model, this is where javascript controls the html
+
+const title = document.getElementById("title");
+const button = document.getElementById("togglebutton");
+
+button.addEventListener("click", function(){
+    if (title.textContent === "OFF"){
+        title.textContent = "ON";
+        title.style.color = "green";
+    }
+    else {
+        title.textContent = "OFF";
+        title.style.color = "red";
+    }
+});
+
+//this way the javascript can interact with the html 
